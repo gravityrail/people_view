a3ef79099442a38ba80f8ac3d8dcf9e88407c1b0
Byron Anderson <byronanderson32@gmail.com>
2013-01-14 11:27:10 -0800

    generated rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NbuildApiConsumer::Application.config.secret_token = '7d6bb03fc8922c930120b34fe6f410fe01f9ff26373486e577520b120f413a0f12da6319a513fc84422b80e41ad825604d2e51a95bc639a1e2a6cde900ddccb9'
